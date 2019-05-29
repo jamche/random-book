1dd1eebb68a5d82d9d7d25f9ba1c2d70b76b8f44
jamieYeung <jamieyyw@gmail.com>
2019-05-29 17:47:57 -0400

    got api call to work and inputted to array, need to change to when select is submitted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(){
+      super();
+      // create initial state
+      this.state = {
+        allBooks:[]
+      }
+    }
+
+    componentDidMount(){
+      const urlFiction = "https://api.nytimes.com/svc/books/v3/lists/current/Hardcover%20Fiction.json?api-key=AYVlgIMoj1SLFq79F0Kbjn46f2eGnERX"
+      const urlNonFiction = "https://api.nytimes.com/svc/books/v3/lists/current/Hardcover%20Nonfiction.json?api-key=AYVlgIMoj1SLFq79F0Kbjn46f2eGnERX"
+      // api call
+      axios({
+        method:'GET',
+        url:urlFiction,
+        dataResponse:'json'
+      }).then(responseFiction => {
+
+        responseFiction = responseFiction.data.results.books
+        console.log(responseFiction)
+        this.setState({
+          allBooks:responseFiction
+        })
+      })
+      axios({
+        method:'GET',
+        url:urlNonFiction,
+        dataResponse:'json'
+      }).then(responseNonFiction=>{
+        responseNonFiction = responseNonFiction.data.results.books
+        console.log(responseNonFiction)
+        this.setState({
+          allBooks:responseNonFiction
+        })
+      })
+    }
+
+    render(){
+      return (
+        <div className="App">
+          <h1>Hey There :)</h1>
+        </div>
+      );
+  }
 }
 
 export default App;
