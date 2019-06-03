12082b5b48d2ce126d01d294c89bf5516ca94bf1
jamieYeung <jamieyyw@gmail.com>
2019-06-02 20:54:24 -0400

    Updates

---
+++ precache-manifest.e2f61d74338fe5f9bdc72c8dd5ed5300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebb4d7d8a73d2be8356e87ba246c9d7",
+    "url": "/jamieYeungRandomBookProject/index.html"
+  },
+  {
+    "revision": "299a3699b0f76372fe28",
+    "url": "/jamieYeungRandomBookProject/static/css/main.30c277ee.chunk.css"
+  },
+  {
+    "revision": "71153f27272d9b8a7efa",
+    "url": "/jamieYeungRandomBookProject/static/js/2.7f0fdec7.chunk.js"
+  },
+  {
+    "revision": "299a3699b0f76372fe28",
+    "url": "/jamieYeungRandomBookProject/static/js/main.9d5da16e.chunk.js"
+  },
+  {
+    "revision": "d22b5009151947cae930",
+    "url": "/jamieYeungRandomBookProject/static/js/runtime~main.8a1af6f5.js"
+  },
+  {
+    "revision": "b3622c0e475ba36e8dd91b47220e1f83",
+    "url": "/jamieYeungRandomBookProject/static/media/library.b3622c0e.jpg"
+  }
+]);
